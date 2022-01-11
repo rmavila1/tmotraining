c4705b2f5bd0fb39feb20084e7c8122602052de4
Remya Mavila <remya.mavila1@t-mobile.com>
2022-01-10 23:13:37 -0800

    implementation

---
+++ src/main/java/com/galvanize/tmo/paspringstarter/Book.java
@@ -0,0 +1,40 @@
+package com.galvanize.tmo.paspringstarter;
+
+public class Book {
+    int id;
+    String author;
+    String title;
+    int yearPublished;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getYearPublished() {
+        return yearPublished;
+    }
+
+    public void setYearPublished(int yearPublished) {
+        this.yearPublished = yearPublished;
+    }
+}
--- src/main/java/com/galvanize/tmo/paspringstarter/LibraryController.java
+++ src/main/java/com/galvanize/tmo/paspringstarter/LibraryController.java
@@ -1,13 +1,37 @@
 package com.galvanize.tmo.paspringstarter;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.*;
 @RestController
 public class LibraryController {
 
+    int id = 1;
+    List<Book> bookList = new ArrayList<Book>();
     @GetMapping("/health")
     public void health() {
+    }
 
+    @GetMapping("/books")
+    public List<Book> getBooks() {
+        return bookList;
+    }
+
+    @PostMapping(value = "/books", produces = "application/json")
+    public ResponseEntity<Book> addBooks(@RequestBody Book book) {
+
+        book.setId(id);
+        bookList.add(book);
+        id++;
+        return new ResponseEntity<Book>(book, HttpStatus.CREATED);
+    }
+
+
+    @DeleteMapping(value = "/books")
+    public ResponseEntity deleteBooks(){
+        bookList.clear();
+        return new ResponseEntity(HttpStatus.NO_CONTENT);
     }
 }
